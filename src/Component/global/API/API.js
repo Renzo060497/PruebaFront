0c60abe9bd8d9632f109937b75c929824ebce11f
renzo <renzo060497@gmail.com>
2019-12-01 15:55:57 -0500

    sdasd

---
+++ src/Component/global/API/API.js
@@ -0,0 +1,5 @@
+export default {    
+    // "url": "https://sigapdev-controlrecibos-back.herokuapp.com/"  
+    "url": "https://backfisicode.herokuapp.com/"    
+    // "url": "http://localhost:7896/"
+}
